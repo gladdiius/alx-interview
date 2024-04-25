9c759288fa438572ad63a6c97f69d1eb86ee2554
elias <eliaseshetu28@gmail.com>
2024-04-25 21:50:35 +0300

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""define min opratinos"""
+
 
 def minOperations(n):
     """
@@ -12,23 +14,13 @@ def minOperations(n):
     - int: Minimum number of operations to achieve 'n' H characters.
            Returns 0 if 'n' is impossible to achieve.
     """
-    
-    # Initialize operations count
     operations = 0
-    
-    # Start with the smallest divisor
     divisor = 2
     
-    # Continue until 'n' is reduced to 1
     while n > 1:
-        # Check if 'n' is divisible by the current divisor
         if n % divisor == 0:
-            # Reduce 'n' by the divisor
             n //= divisor
-            # Increase operations by the divisor (Copy operation)
             operations += divisor
         else:
-            # If not divisible, try the next divisor
             divisor += 1
-    
     return operations
