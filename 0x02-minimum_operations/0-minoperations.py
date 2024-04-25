28c7774d19dfafebf21ddb849263952b5b2a2d2c
elias <eliaseshetu28@gmail.com>
2024-04-25 21:25:40 +0300

    commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""define min_opration"""
+
+def min_operations(n):
+    """
+    Calculate the minimum number of operations needed to obtain 'n' H characters 
+    in the text file using the operations 'Copy All' and 'Paste'.
+    
+    Args:
+    - n (int): The desired number of H characters.
+    
+    Returns:
+    - int: Minimum number of operations to achieve 'n' H characters.
+           Returns 0 if 'n' is impossible to achieve.
+    """
+    operations = 0
+    
+    divisor = 2
+    
+    while n > 1:
+        if n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        else:
+            divisor += 1
+    
+    return operations
