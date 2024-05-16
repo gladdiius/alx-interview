a8794c1c28d7adc02157b74a63fe59b48e9ddfcf
elias <eliaseshetu2847@gmail.com>
2024-05-16 21:42:23 +0300

    0-nqueens.py

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+import sys
+"""
+    Check if it's safe to place a queen at position (row, col) on the board.
+"""
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen at position (row, col) on the board.
+    """
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_n_queens_util(board, col, n, solutions):
+    """
+    Utility function to solve the N-Queens problem recursively.
+    """
+    if col == n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return True
+
+    res = False
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            res = solve_n_queens_util(board, col + 1, n, solutions) or res
+            board[i][col] = 0
+    return res
+
+
+def solve_n_queens(n):
+    """
+    Main function to solve the N-Queens problem.
+    """
+    if not n.isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(n)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0] * n for _ in range(n)]
+
+    solutions = []
+
+    solve_n_queens_util(board, 0, n, solutions)
+
+    for sol in solutions:
+        print(sol)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    solve_n_queens(sys.argv[1])
