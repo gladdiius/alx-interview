ed4b176cd86cf2b41e3d9619d6adcc224c0e1cfb
elias <eliaseshetu2847@gmail.com>
2024-05-23 16:15:59 +0300

    0-starwars_

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+get_movie_data() {
+  movie_id=$1
+  response=$(curl -s "https://swapi.dev/api/films/$movie_id/")
+  if [[ $(echo "$response" | jq -r '.detail') == "Not found" ]]; then
+    echo "Error: Movie ID $movie_id not found."
+    exit 1
+  else
+    echo "$response"
+  fi
+}
+
+get_character_name() {
+  character_url=$1
+  response=$(curl -s "$character_url")
+  character_name=$(echo "$response" | jq -r '.name')
+  echo "$character_name"
+}
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <Movie ID>"
+  exit 1
+fi
+
+movie_id=$1
+movie_data=$(get_movie_data "$movie_id")
+character_urls=$(echo "$movie_data" | jq -r '.characters[]')
+
+for url in $character_urls; do
+  get_character_name "$url"
+done
