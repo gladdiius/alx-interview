4c5367f237667a3685a81cf34c338667c9aa5348
elias <eliaseshetu2847@gmail.com>
2024-06-06 20:14:42 +0300

    commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,11 +19,14 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)  # Sort the coins list in descending order
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+        if coin <= total:
+            for i in range(coin, total + 1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
