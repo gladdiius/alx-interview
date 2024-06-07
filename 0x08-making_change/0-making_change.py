28c375d328bc6a8b755b27e2e188467dc38c1d99
elias <eliaseshetu2847@gmail.com>
2024-06-07 10:34:11 +0300

    0 making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,14 +19,14 @@ def makeChange(coins, total):
         return 0
 
     coins.sort(reverse=True)
-    
+
     num_coins = 0
     for coin in coins:
         if total >= coin:
             count = total // coin
             num_coins += count
             total -= count * coin
-    
+
     if total != 0:
         return -1
     return num_coins
